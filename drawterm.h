7947619fbe0f4b89332b57c67956867f6b5d704b
cinap_lenrek <cinap_lenrek@felloff.net>
2017-01-15 22:12:32 +0100

    set kerndate in main(), move global variables to cpu.c

--- drawterm.h
+++ drawterm.h
@@ -1,10 +1,8 @@
 extern int havesecstore(char *addr, char *owner);
 extern char *secstore;
-extern char secstorebuf[65536];
 extern char *secstorefetch(char *addr, char *owner, char *passwd);
 extern char *authserver;
 extern int exportfs(int);
-extern char *user;
 extern int dialfactotum(void);
 extern char *getuser(void);
 extern void cpumain(int, char**);
