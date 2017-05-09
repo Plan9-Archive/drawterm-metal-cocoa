10fd7e70867c24d9fed66515af26017175258aa1
cinap_lenrek <cinap_lenrek@felloff.net>
2017-05-09 22:19:58 +0200

    allocate 2 ulongs for secalloc() to keep it 8 byte aligned (use first ulong for magic constant)
    on sparc64, secalloc needs to return 8 byte aligned pointer,
    so we allocate two ulongs instead of one and use the first
    one for the magic constant 0x5ECA110C.

--- kern/alloc.c
+++ kern/alloc.c
@@ -16,19 +16,25 @@ malloc(ulong n)
 	return mallocz(n, 1);
 }
 
+enum {
+	SECMAGIC = 0x5ECA110C,
+};
+
 void*
 secalloc(ulong n)
 {
-	void *p = mallocz(n+sizeof(ulong), 1);
-	*(ulong*)p = n;
-	return (ulong*)p+1;
+	void *p = mallocz(n+sizeof(ulong)*2, 1);
+	((ulong*)p)[0] = SECMAGIC;
+	((ulong*)p)[1] = n;
+	return (ulong*)p+2;
 }
 
 void
 secfree(void *p)
 {
 	if(p != nil){
+		assert(((ulong*)p)[-2] == SECMAGIC);
 		memset(p, 0, ((ulong*)p)[-1]);
-		free((ulong*)p-1);
+		free((ulong*)p-2);
 	}
 }
