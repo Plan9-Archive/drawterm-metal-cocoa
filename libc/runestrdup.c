91d5706eb0db886724e5635c97d1a3be09ca29be
cinap_lenrek <cinap_lenrek@felloff.net>
2017-12-31 11:38:01 +0100

    libc: update various libc functions (from 9front)

--- libc/runestrdup.c
+++ libc/runestrdup.c
@@ -2,13 +2,13 @@
 #include <libc.h>
 
 Rune*
-runestrdup(Rune *s) 
-{  
+runestrdup(Rune *s)
+{
 	Rune *ns;
 
 	ns = malloc(sizeof(Rune)*(runestrlen(s) + 1));
 	if(ns == 0)
 		return 0;
-
+	setmalloctag(ns, getcallerpc(&s));
 	return runestrcpy(ns, s);
 }
